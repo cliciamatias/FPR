8accbd6ee4fd1af843a1a13012f60dcb6926bf0b
Clicia Matias <103210015+cliciamatias@users.noreply.github.com>
2023-09-17 00:25:51 -0300

    Update and rename 6.5 - m1 inteiros m2 aleatorios e m3 com soma de L e C.c to 6.5 - m1 aleatórios m2 0 e 1 e m3 com soma de L e C.c

--- lista/6.5 - m1 aleatórios m2 0 e 1 e m3 com soma de L e C.c
+++ lista/6.5 - m1 aleatórios m2 0 e 1 e m3 com soma de L e C.c
@@ -1,8 +1,8 @@
-/*Questão 05:
-Desenvolver uma função que receba duas matrizes de inteiros, ambas de ordem 10, com as seguintes características:
-a. M1: matriz contendo números inteiros quaisquer;
+/*QuestÃ£o 05:
+Desenvolver uma funÃ§Ã£o que receba duas matrizes de inteiros, ambas de ordem 10, com as seguintes caracterÃ­sticas:
+a. M1: matriz contendo nÃºmeros inteiros quaisquer;
 b. M2: matriz contendo apenas os valores 0 ou 1.
-Desenvolver uma função que a partir destas matrizes gere a matriz M3, também de ordem 10, cujos elementos são
+Desenvolver uma funÃ§Ã£o que a partir destas matrizes gere a matriz M3, tambÃ©m de ordem 10, cujos elementos sÃ£o
 definidos da seguinte forma:
 M2ij = 0 --> M3ij = M1ij
 M2ij = 1 --> M3ij = soma dos elementos da linha i com os da coluna j da matriz.*/
@@ -40,15 +40,15 @@ void gerarM3(int m1[L][C], int m2[L][C], int m3[L][C])
 	{
 		for(j=0; j<C; j++)
 		{
-			//verificando se o elemento de m2 é '0' e atribuindo em m3 o elemento de m1
+			//verificando se o elemento de m2 Ã© '0' e atribuindo em m3 o elemento de m1
 			if(m2[i][j] == 0)
 			{
 				m3[i][j] = m1[i][j];
 			}
 			else
 			{
-				//caso contrário, m3 recebe a soma das linhas e colunas de m1
-				m3[i][j] = somaLinhaColuna(m1,i,j); //chamando a função com resultado 
+				//caso contrÃ¡rio, m3 recebe a soma das linhas e colunas de m1
+				m3[i][j] = somaLinhaColuna(m1,i,j); //chamando a funÃ§Ã£o com resultado 
 			}
 		}
 	}
