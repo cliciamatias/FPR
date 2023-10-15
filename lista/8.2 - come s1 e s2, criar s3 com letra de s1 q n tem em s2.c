45ee5e9267abf95ceae3ed5c4bc729c40510c1c6
Clicia Matias <103210015+cliciamatias@users.noreply.github.com>
2023-10-15 01:16:19 -0300

    Add files via upload

--- lista/8.2 - come s1 e s2, criar s3 com letra de s1 q n tem em s2.c
+++ lista/8.2 - come s1 e s2, criar s3 com letra de s1 q n tem em s2.c
@@ -18,23 +18,35 @@ void main()
 	printf("\ns2: %s", string2);
 	//chamando a função
 	caracterS3(string1, string2, string3);
+	printf("\ns3: %s", string3);
 }
 void caracterS3(char s1[], char s2[], char s3[])
 {
-	int i, j;
+	int i, j, encontrou;
+	int p=0; //posições de 's3'
+	
+	//inicializando 's3' vazia
+	s3[0] = '\0';
 	
 	//percorrendo a string 1
 	for(i=0; s1[i]; i++)
 	{
-		//percorrendo as posições da string 1
-		for(j=0; j<i; j++)
+		encontrou = 0; //'encontrou' para indicar se o caractere foi encontrado em 's2'
+		//percorrendo as posições da string 2
+		for(j=0; s2[j]; j++)
 		{
-			//se o elemento de s1 é igual ao de s2(na posição j, comparando às letras de s1[i]), para passar os elementos iguais para s3
+			//se o elemento de s1 é igual ao de s2(na posição j, comparando às letras de s1[i])
 			if(s1[i] == s2[j])
 			{
-				s3[i] = s1[i];
+				encontrou = 1;
 			}
 		}
+		//caracteres de 's1' não for encontrado em 's2', passar caracteres de 's1' para 's3'
+		if(!encontrou)
+		{
+			s3[p++] = s1[i];
+		}
 	}
-	printf("\ns3: %s ", s3[j]);
+	//encerrando 's3' com o caractere nulo
+    s3[p] = '\0';
 }
