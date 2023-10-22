74b7e8b4669a0ecfa6fcd6d9be96010924209b9f
Clicia Matias <103210015+cliciamatias@users.noreply.github.com>
2023-10-22 15:41:32 -0300

    Update and rename 1.1 - matriz - parte 1.c to 1.1 - matriz diagonal principal- parte 1.c

--- provas/2023.1/1.1 - matriz diagonal principal- parte 1.c
+++ provas/2023.1/1.1 - matriz diagonal principal- parte 1.c
@@ -1,11 +1,11 @@
-/*Quest„o 01 [2,5 pontos]:
-Considerando a existÍncia de uma matriz de inteiros M, de ordem N (ou seja, o n∞ de linhas e o n∞ de colunas s„o iguais
-a N), onde N È uma constante, desenvolver uma funÁ„o que retorne 1 se a seguinte regra for atendida por M (caso
-contr·rio, o valor 0 dever· ser retornado):
-? A soma dos elementos da diagonal principal deve ser igual ‡ soma dos elementos que est„o acima desta
-diagonal, assim como ‡ soma dos elementos abaixo da diagonal.
+/*Quest√£o 01 [2,5 pontos]:
+Considerando a exist√™ncia de uma matriz de inteiros M, de ordem N (ou seja, o n¬∞ de linhas e o n¬∞ de colunas s√£o iguais
+a N), onde N √© uma constante, desenvolver uma fun√ß√£o que retorne 1 se a seguinte regra for atendida por M (caso
+contr√°rio, o valor 0 dever√° ser retornado):
+? A soma dos elementos da diagonal principal deve ser igual √† soma dos elementos que est√£o acima desta
+diagonal, assim como √† soma dos elementos abaixo da diagonal.
 Exemplo 1:
-Para a seguinte matriz, a funÁ„o retornar· 1:
+Para a seguinte matriz, a fun√ß√£o retornar√° 1:
 6 3 1 2 6
 1 3 5 4 2
 3 4 4 2 2
@@ -13,7 +13,7 @@ Para a seguinte matriz, a funÁ„o retornar· 1:
 4 0 2 1 8
 
 Exemplo 2:
-Para a seguinte matriz, a funÁ„o retornar· 0:
+Para a seguinte matriz, a fun√ß√£o retornar√° 0:
 1 8 5 9 4
 2 5 4 6 5
 4 1 2 3 6
@@ -33,7 +33,7 @@ void main()
 						{4, 0, 2, 1, 8}};
 	exibir(matriz); //exibindo a matriz original
 	
-	//chamando a funÁ„o
+	//chamando a fun√ß√£o
 	retorno = somaDiagonal(matriz);
 	
 	//verificando retorno e exibindo o resultado
@@ -76,7 +76,7 @@ int somaDiagonal(int matriz[N][N])
 			}
 		}
 	}
-	//verificando se a soma dos elementos de acima e abaixo da diagonal È igual ‡ diagonal principal
+	//verificando se a soma dos elementos de acima e abaixo da diagonal √© igual √† diagonal principal
 	if((somaCima == somaDiagonal) && (somaBaixo == somaDiagonal))
 	{
 		return 1; //retornando 1 pois atende ao requisito
