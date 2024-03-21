6002c8e8bd0be15753cf7493234af346fdeb6b3d
Clicia Matias <103210015+cliciamatias@users.noreply.github.com>
2024-03-21 08:11:39 -0300

    Atualizar o 4.1 - função strcmp.c

--- lista/4.1 - função strcmp.c
+++ lista/4.1 - função strcmp.c
@@ -1,7 +1,7 @@
-/*/*Quest„o 01:
-Suponha que n„o existissem as funÁıes strlen,
-strcpy, strcat e strcmp. Pede-se, ent„o, a
-implementaÁ„o de funÁıes que tenham o
+/*/*Quest√£o 01:
+Suponha que n√£o existissem as fun√ß√µes strlen,
+strcpy, strcat e strcmp. Pede-se, ent√£o, a
+implementa√ß√£o de fun√ß√µes que tenham o
 mesmo objetivo daquelas.*/
 
 #include <stdio.h>
@@ -9,66 +9,72 @@ mesmo objetivo daquelas.*/
 int funcaoStrcmp(char s1[], char s2[]);
 void main()
 {
-	char s1[20], s2[20];
-	int retorno;
-	
-	printf("Digite a primeira String: ");
-	fflush(stdin);
-	gets(s1);
-	
-	printf("Digite a segunda String: ");
-	fflush(stdin);
-	gets(s2);
-	
-	//chamando a funÁ„o STRCMP
-	retorno = funcaoStrcmp(s1,s2);
-	
-	if(retorno == 0)
-	{
-		printf("\nAs strings sao iguais.\nRetorno: %d.",retorno);
-	}
-	else
-	{
-		if(retorno == -1)
-		{
-			printf("\nA string %s eh maior que a string %s.\nRetorno: %d.",s1,s2,retorno);
-		}
-		else
-		{
-			printf("\nA string %s eh menor que a string %s.\nRetorno: %d.",s2,s1,retorno);
-		}
-	}
+        char s1[20], s2[20];
+        int retorno;
+
+        printf("Digite a primeira String: ");
+        fflush(stdin);
+        gets(s1);
+
+        printf("Digite a segunda String: ");
+        fflush(stdin);
+        gets(s2);
+
+        //chamando a fun√ß√£o STRCMP
+        retorno = funcaoStrcmp(s1,s2);
+
+        if(retorno == 0)
+        {
+                printf("\nAs strings sao iguais.\nRetorno: %d.",retorno);
+        }
+        else
+        {
+                if(retorno < 0)
+                {
+                        printf("\nA string %s vem antes da string %s alfabeticamente.\nRetorno: %d.",s1,s2,retorno);
+                }
+                else
+                {
+                        printf("\nA string %s vem antes da string %s alfabeticamente.\nRetorno: %d.",s2,s1,retorno);
+                }
+        }
 }
 //STRCMP - COMPARAR STRING ('s1'>'s2' = -1; 's1'=='s2' = 0; 's1'>'s2' = 1)
 int funcaoStrcmp(char s1[], char s2[])
 {
-	int i;
-	
-	for(i=0;(s1[i]==s2[i])&&(s1[i])&&(s2[i]); i++)
-	{
-		if(s1[i] == s2[i])
-		{
-			return 0;
-		}
-		else
-		{
-			if(s1[i] > s2[i])
-			{
-				return -1;
-			}
-			else
-			{
-				return 1;
-			}
-		}
-	}
+        int i;
+
+        for(i=0; s1[i] && s2[i] && (s1[i]==s2[i]); i++);
+        
+        return (s1[i]-s2[i]);
+        
+        
+        
+       /* 
+        {
+                if(s1[i] == s2[i])
+                {
+                        return 0;
+                }
+                else
+                {
+                        if(s1[i] > s2[i])
+                        {
+                                return -1;
+                        }
+                        else
+                        {
+                                return 1;
+                        }
+                }
+        }*/
 }
 
 /*
-	//STRCMP - COMPARAR STRING ('s1'>'s2' = -1; 's1'=='s2' = 0; 's1'>'s2' = 1)
-	char s1[] = "Muito";
-	char s2[] = "Bela";
-	int resultado = strcmp(s1,s2);
-	
-	printf("O resultado da comparacao eh %d", resultado);
+        //STRCMP - COMPARAR STRING ('s1'>'s2' = -1; 's1'=='s2' = 0; 's1'>'s2' = 1)
+        char s1[] = "Muito";
+        char s2[] = "Bela";
+        int resultado = strcmp(s1,s2);
+        
+        printf("O resultado da comparacao eh %d", resultado);
 */
