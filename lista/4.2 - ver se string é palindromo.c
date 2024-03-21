b1660624dc96f899321ca8e43299d1885303f758
Clicia Matias <103210015+cliciamatias@users.noreply.github.com>
2024-03-21 08:41:16 -0300

    Atualizar o 4.2 - ver se string é palindromo.c

--- lista/4.2 - ver se string é palindromo.c
+++ lista/4.2 - ver se string é palindromo.c
@@ -11,34 +11,45 @@ esquerda para direita ou vice-versa.*/
 int palindromo(char s[]);
 void main()
 {
-	char s[20] = "alaa"; //EH palindromo
-	int retorno = palindromo(s); //chamando a função
-	
-	//verificando o retorno e exibindo o resultado
-	if(retorno == 1)
-	{
-		printf("(%s) EH palindromo.", s);
-		
-	}
-	else
-	{
-		printf("(%s) NAO eh palindromo.", s);
-	}
+        char s[20] = "ala"; //EH palindromo
+        int retorno = palindromo(s); //chamando a função
+
+        //verificando o retorno e exibindo o resultado
+        if(retorno == 1)
+        {
+                printf("(%s) EH palindromo.", s);
+
+        }
+        else
+        {
+                printf("(%s) NAO eh palindromo.", s);
+        }
 }
 int palindromo(char s[])
 {
-	int i, tamanho = strlen(s);
-	
-	//percorrendo a string 's'
-	for(i=0; s[i]; i++)
-	{
-		//verificando se o ultimo caracter é igual ao primeiro e assim por diante
-		if(s[i] != s[tamanho-i-1])
-		{
-			return 0; //retornando '0' pois se os caracteres não forem iguais não é um palindromo
-		}
-	}
-	return 1; //retornando 1, pois se saiu do for é palindromo
+        int i, j;
+
+
+
+
+        for(i=0, j=strlen(s)-1; i<j; i++,j--)
+        {
+            //verificando se o ultimo caracter é igual ao primeiro e assim por diante
+                if(s[i] != s[j])
+                {
+                        return 0; //retornando '0' pois se os caracteres não forem iguais não é um palindromo
+                }
+        }return 1;
+        //percorrendo a string 's'
+    /*    for(i=0; s[i]; i++)
+        {
+                //verificando se o ultimo caracter é igual ao primeiro e assim por diante
+                if(s[i] != s[tamanho-i-1])
+                {
+                        return 0; //retornando '0' pois se os caracteres não forem iguais não é um palindromo
+                }
+        }
+        return 1;*/ //retornando 1, pois se saiu do for é palindromo
 }
 /*
 #include <stdio.h>
@@ -46,31 +57,31 @@ int palindromo(char s[])
 int palindromo(char s[]);
 void main()
 {
-	char s[20] = "osso";
-	int retorno = palindromo(s); //chmando a funcao
-	
-	if(retorno == 1)
-	{
-		printf("'%s' eh palindromo.\n", s);
-	}
-	else
-	{
-		printf("'%s' nao eh palindromo.", s);
-	}
+        char s[20] = "osso";
+        int retorno = palindromo(s); //chmando a funcao
+        
+        if(retorno == 1)
+        {
+                printf("'%s' eh palindromo.\n", s);
+        }
+        else
+        {
+                printf("'%s' nao eh palindromo.", s);
+        }
 }
 int palindromo(char s[])
 {
-	int i, tamanho=strlen(s);
-	
-	//percorendo a string até a sua matade, pois é espelhado se da direita ou esquerda forem os mesmos caracteres
-	for(i=0; i<tamanho/2; i++)
-	{
-		//verificando se a string é a mesma de trás para frente
-		if(s[i] != s[tamanho-i-1]) 
-		{
-           	return 0; //retornando '0' pois se os caracteres não forem iguais não é um palindromo
+        int i, tamanho=strlen(s);
+        
+        //percorendo a string até a sua matade, pois é espelhado se da direita ou esquerda forem os mesmos caracteres
+        for(i=0; i<tamanho/2; i++)
+        {
+                //verificando se a string é a mesma de trás para frente
+                if(s[i] != s[tamanho-i-1]) 
+                {
+                   return 0; //retornando '0' pois se os caracteres não forem iguais não é um palindromo
         }
-	}
-	return 1; //retornando 1, pois se saiu do for é palindromo
+        }
+        return 1; //retornando 1, pois se saiu do for é palindromo
 }
 */
