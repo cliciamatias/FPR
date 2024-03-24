d2b03dbae82acd1baf3342ccae8653adb0a58730
Clicia Matias <103210015+cliciamatias@users.noreply.github.com>
2024-03-24 20:24:26 -0300

    Update 5.4 - determinar se matriz é simétrica.c

--- lista/5.4 - determinar se matriz é simétrica.c
+++ lista/5.4 - determinar se matriz é simétrica.c
@@ -1,6 +1,68 @@
-/*QUEST�O 04:
-Fazer uma fun��o que, dada uma matriz M6�6,
-determine se ela � sim�trica.*/
+/*QUESTÃO 04:
+Fazer uma função que, dada uma matriz M6×6,
+determine se ela é simétrica.*/
+#include <stdio.h>
+void preencherMatriz(int lc, int m[lc][lc]);
+void main()
+{
+	int lc=6, retorno;
+	int m[lc][lc];
+	
+	preencherMatriz(lc,m); //exibindo a matriz
+	retorno = simetrica(lc,m); //chamando a função que verifica se é simétrica ou não
+	
+	if(retorno == 1)
+	{
+		printf("\nEh uma matriz simetrica.");
+	}					
+	else
+	{
+		printf("\nNAO eh uma matriz simetrica.");
+	}
+}
+
+int simetrica(int lc, int m[lc][lc])
+{
+	int i, j;
+	
+	//percorrendo as linhas da matriz
+	for(i=0; i<lc; i++)
+	{
+		//percorrendo as colunas da matriz
+		for(j=0; j<lc; j++)
+		{
+			//verificando se seus opostos sao diferentes, pois se houver algum já não será simétrica
+			if(m[i][j] != m[j][i])
+			{
+				return 0; //a matriz não é simétrica
+			}
+		}
+	}
+	return 1; //a matriz é simétrica
+}
+
+void preencherMatriz(int lc, int m[lc][lc])
+{
+	int i, j;
+	srand(time(NULL));
+	for(i=0; i<lc; i++)
+	{
+		for(j=0; j<lc; j++)
+		{
+			m[i][j] = rand()%9;
+			printf("%d ", m[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+
+
+//------------------------------------------------------------------
+/*QUESTÃO 04:
+Fazer uma função que, dada uma matriz M6×6,
+determine se ela é simétrica.*/
+/*
 #include <stdio.h>
 int matrizSimetrica(int n, int m[n][n]);
 void exibir(int n, int m[n][n]);
@@ -13,10 +75,10 @@ void main()
 	//exibindo a matriz
 	exibir(n, matriz);	
 	
-	//chamando a fun��o
+	//chamando a função
 	retorno = matrizSimetrica(n,matriz);
 	
-	//verificando o retorno da fun��o
+	//verificando o retorno da função
 	if(retorno == 1)
 	{
 		printf("\nEssa matriz EH simetrica.");
@@ -36,10 +98,10 @@ int matrizSimetrica(int n, int m[n][n])
 		//percorrendo as colunas da matriz
 		for(j=0; j<n; j++)
 		{
-			//verificando se elementos 'espelhados'(inverso de linhas e colunas) da matriz s�o difetentes
+			//verificando se elementos 'espelhados'(inverso de linhas e colunas) da matriz são difetentes
 			if(m[i][j] != m[j][i])
 			{
-				return 0;  //retornando 0, pois h� elemento diferentes no espelhamento da matriz
+				return 0;  //retornando 0, pois há elemento diferentes no espelhamento da matriz
 			}
 		}
 	}
@@ -56,4 +118,4 @@ void exibir(int n, int m[n][n])
 		}
 		printf("\n");
 	}	
-}
+}*/
