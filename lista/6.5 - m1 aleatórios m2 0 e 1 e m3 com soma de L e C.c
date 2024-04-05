b4b0b9176fd41727a844d08142b0e6cb6257431c
Clicia Matias <103210015+cliciamatias@users.noreply.github.com>
2024-04-05 19:20:07 -0300

    Update 6.5 - m1 aleatórios m2 0 e 1 e m3 com soma de L e C.c

--- lista/6.5 - m1 aleatórios m2 0 e 1 e m3 com soma de L e C.c
+++ lista/6.5 - m1 aleatórios m2 0 e 1 e m3 com soma de L e C.c
@@ -7,6 +7,109 @@ definidos da seguinte forma:
 M2ij = 0 --> M3ij = M1ij
 M2ij = 1 --> M3ij = soma dos elementos da linha i com os da coluna j da matriz.*/
 #include <stdio.h>
+void preencherM1(int n, int m1[n][n]);
+void preencherM2(int n, int m2[n][n]);
+void matrizM3(int n, int m1[n][n], int m2[n][n], int m3[n][n]);
+void exibir(int n, int m[n][n]);
+void main()
+{
+	int n=5;
+	int m1[n][n], m2[n][n], m3[n][n]; 
+	
+	printf("Matriz M1:\n");
+	preencherM1(n, m1);
+	exibir(n, m1);
+	
+	printf("\nMatriz M2:\n");
+	preencherM2(n, m2);
+	exibir(n, m2);
+	
+	printf("\nMatriz M3:\n");
+	matrizM3(n, m1, m2, m3);
+	exibir(n, m3);
+}
+
+void matrizM3(int n, int m1[n][n], int m2[n][n], int m3[n][n])
+{
+	int i, j, k, soma;
+	
+	//percorrendo a matriz
+	for(i=0; i<n; i++)
+	{
+		for(j=0; j<n; j++)
+		{ 
+			//M2ij = 0 --> M3ij = M1ij
+			if(m2[i][j] == 0)
+			{
+				m3[i][j] = m1[i][j]; //se o elemento de 'm2' for 0, m3 receberá elementos de 's1'
+			}
+			//M2ij = 1 --> M3ij = soma dos elementos da linha i com os da coluna j da matriz
+			if(m2[i][j] == 1)
+			{
+				soma = 0; //zerando a soma para renovar a cada repetição
+				for(k=0; k<n; k++)
+				{
+		 			//somando os elementos das linhas e colunas da matriz 
+					soma += m1[i][k];
+					soma += m1[k][j];	
+				}
+				soma -= m1[i][j]; //decrementando os um dos valores da matriz principal somados duas vezes anteriormente
+				m3[i][j] = soma; // quando 'm2' for 1, a posição em 'm3' receberá a soma dos elementos de 'm1'
+			}
+		}
+	}	
+}
+void preencherM1(int n, int m1[n][n])
+{
+	int i, j;
+	srand(time(NULL));
+	for(i=0; i<n; i++)
+	{
+		for(j=0; j<n; j++)
+		{
+			m1[i][j] = rand()%6;
+		}
+	}
+}
+void preencherM2(int n, int m2[n][n])
+{
+	int i, j;
+	srand(time(NULL));
+	for(i=0; i<n; i++)
+	{
+		for(j=0; j<n; j++)
+		{
+			m2[i][j] = rand()%2;
+		}
+	}
+}
+void exibir(int n, int m[n][n])
+{
+	int i, j;
+	for(i=0; i<n; i++)
+	{
+		for(j=0; j<n; j++)
+		{
+			printf("|%d|", m[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+
+
+
+//---------------------------------------------------------------------------------------------------------------
+/*Questão 05:
+Desenvolver uma função que receba duas matrizes de inteiros, ambas de ordem 10, com as seguintes características:
+a. M1: matriz contendo números inteiros quaisquer;
+b. M2: matriz contendo apenas os valores 0 ou 1.
+Desenvolver uma função que a partir destas matrizes gere a matriz M3, também de ordem 10, cujos elementos são
+definidos da seguinte forma:
+M2ij = 0 --> M3ij = M1ij
+M2ij = 1 --> M3ij = soma dos elementos da linha i com os da coluna j da matriz.*/
+/*
+#include <stdio.h>
 #include <time.h>
 #define L 5
 #define C 5
@@ -108,4 +211,4 @@ void exibirMatriz(int m[L][C])
 		printf("\n");
 	}
 	printf("\n");
-}
+}*/
