754c13dc2f93a9dfc2b99adb72c3f4f5d699f42f
Clicia Matias <103210015+cliciamatias@users.noreply.github.com>
2024-05-01 21:01:56 -0300

    Update 9.3 - multiplos de N inferiores iguais a V - recursivo.c

--- lista/9.3 - multiplos de N inferiores iguais a V - recursivo.c
+++ lista/9.3 - multiplos de N inferiores iguais a V - recursivo.c
@@ -1,25 +1,55 @@
-/*Questão 03:
-Desenvolver uma função recursiva que exiba
-todos os múltiplos do número N, inferiores ou
+/*QuestÃ£o 03:
+Desenvolver uma funÃ§Ã£o recursiva que exiba
+todos os mÃºltiplos do nÃºmero N, inferiores ou
 iguais ao valor V.*/
+#include <stdio.h>
+void recursiva(int n, int v, int multiplos);
+void main()
+{
+	int n=5, v=30, multiplos=0;
+	
+	recursiva(n, v, multiplos); //chamando a funÃ§Ã£o recursiva
+}
+void recursiva(int n, int v, int multiplos)
+{
+	if(multiplos <= v) //CASO BASE - verificando se multiplos forem inferiores ou menores ao valor 'v'
+	{
+		if(multiplos % n == 0) //CASO GERAL - se for multiplo de 'n', exibir
+		{
+			printf("%d ", multiplos);
+		}
+		recursiva(n, v, multiplos+1); //chamando a funÃ§Ã£o recursiva com 'multiplos+1' para verificar cada numero atÃ© 'v'
+	}
+}
+
+
+
+
+
+//--------------------------------------------------------------------------------------------------------------
+/*QuestÃ£o 03:
+Desenvolver uma funÃ§Ã£o recursiva que exiba
+todos os mÃºltiplos do nÃºmero N, inferiores ou
+iguais ao valor V.*/
+/*
 #include <stdio.h>
 void exibirMultiplos(int n, int v, int i);
 void main()
 {
     int n, v;
 
-    printf("Digite o número N: ");
+    printf("Digite o nÃºmero N: ");
     scanf("%d", &n);
 
     printf("Digite o valor V: ");
     scanf("%d", &v);
 
     printf("\nMultiplos de %d inferiores ou iguais a %d:\n", n, v);
-    exibirMultiplos(n, v, 0); //'i' são os multiplos, começando por 0
+    exibirMultiplos(n, v, 0); //'i' sÃ£o os multiplos, comeÃ§ando por 0
 }
 void exibirMultiplos(int n, int v, int i) 
 {
-	//verificando se o multiplo é menor ou igual a 'v'
+	//verificando se o multiplo Ã© menor ou igual a 'v'
     if(i <= v)
 	{
 		//verificando os multiplos de 'n'
@@ -28,6 +58,6 @@ void exibirMultiplos(int n, int v, int i)
 			//exibindo o resultado
             printf("%d ", i);
         }
-        exibirMultiplos(n, v, i + 1); //enquanto o multiplo de 'n' for <= 'v', soma 'i' com 1 para verificar se cada numero até 'v' é multiplo
+        exibirMultiplos(n, v, i + 1); //enquanto o multiplo de 'n' for <= 'v', soma 'i' com 1 para verificar se cada numero atÃ© 'v' Ã© multiplo
     }
-}
+}*/
