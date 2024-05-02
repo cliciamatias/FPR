23d69a2562926d5aa0983de9b1580b387a70ff12
Clicia Matias <103210015+cliciamatias@users.noreply.github.com>
2024-05-01 22:32:13 -0300

    Update 9.6 - ver se string é palindromo - recursivo.c

--- lista/9.6 - ver se string é palindromo - recursivo.c
+++ lista/9.6 - ver se string é palindromo - recursivo.c
@@ -4,6 +4,49 @@ recursiva que determine se s é ou não um
 palíndromo.*/
 #include <stdio.h>
 #include <string.h>
+int recursivo(char s[], int inicio, int tamanho);
+void main()
+{
+	char s[10] = "osso";
+	int i=0, j=strlen(s);
+	int retorno = recursivo(s, i, j-1);
+	
+	if(retorno == 1)
+	{
+		printf("\n'%s' eh palindromo.", s);
+	}
+	else
+	{
+		printf("\n'%s' nao eh palindromo.", s);
+	}
+}
+int recursivo(char s[], int inicio, int tamanho)
+{
+	if(inicio >= tamanho) // CASO BASE - verificando se o 'inicio' é maior ou igual 'tamanho' da string 's' para rtornar que é palindromo
+	{
+		return 1;
+	}
+	if(s[inicio] == s[tamanho]) //CASO GERAL - verificando se o incio e o fim da string sao iguais, pois na chamada da função 'incio+' e 'tamanho-1'
+	{
+		recursivo(s, inicio+1, tamanho-1); //chamando a função recursiva
+	}
+	else
+	{
+		return 0; //quando os caracteres espelhados de 's' forem diferentes não eh palindromo
+	}
+}
+
+
+
+
+// --------------------------------------------------------------------------------------------------------------
+/*Questão 06:
+Dada uma string s, desenvolver uma função
+recursiva que determine se s é ou não um
+palíndromo.*/
+/*
+#include <stdio.h>
+#include <string.h>
 int stringPalindromo(char s[], int inicio, int fim);
 void main()
 {
@@ -29,18 +72,14 @@ int stringPalindromo(char s[], int inicio, int fim)
 	{
 		return 1;
 	}
-        else
+	//se o inicio da string for igual ao fim, verificar se é palíndromo
+	if(s[inicio] == s[fim])
 	{
-		//se o inicio da string for igual ao fim, verificar se é palíndromo
-		if(s[inicio] == s[fim])
-		{
-			//somando o inicio e subtraindo o fim para ver se os caracteres "espelhados" de cada lado são iguais, pois será palíndromo
-			stringPalindromo(s, inicio+1, fim-1);
-		}
-		else
-		{
-			return 0; //retornando 0, pois não é palíndromo
-		}
-
+		//somando o inicio e subtraindo o fim para ver se os caracteres "espelhados" de cada lado são iguais, pois será palíndromo
+		stringPalindromo(s, inicio+1, fim-1);
 	}
-}
+	else
+	{
+		return 0; //retornando 0, pois não é palíndromo
+	}
+} */
